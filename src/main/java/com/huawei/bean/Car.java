fca66ad935c21c7d6a892fa5baddcc91c3b2414d
Carvendish <17302010080@fudan.edu.cn>
2019-03-26 17:45:15 +0800

    add route

--- src/main/java/com/huawei/bean/Car.java
+++ src/main/java/com/huawei/bean/Car.java
@@ -1,11 +1,14 @@
 package com.huawei.bean;
 
+import java.util.ArrayList;
+
 public class Car {
     private int id;
     private Cross from;//始发地和目的地似乎都是路口
     private Cross to;
     private int speed;
     private int startTime;
+    private ArrayList<Road> Route;
 
     public Car(int id, Cross from, Cross to, int speed, int startTime) {
         this.id = id;
@@ -13,6 +16,19 @@ public class Car {
         this.to = to;
         this.speed = speed;
         this.startTime = startTime;
+        Route = new ArrayList<>();
+    }
+
+    public void addRoad(Road road){
+        Route.add(road);
+    }
+
+    public ArrayList<Road> getRoute(){
+        return Route;
+    }
+
+    public void setRoute(ArrayList<Road> Route){
+        this.Route = Route;
     }
 
     public int getId() {
