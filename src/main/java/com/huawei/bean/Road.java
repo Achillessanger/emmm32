4919f13cebd54e2438ef6c201922c68ad8dd3a80
achillessanger <1359848409@qq.com>
2019-03-27 12:19:31 +0800

    加了双向路

--- src/main/java/com/huawei/bean/Road.java
+++ src/main/java/com/huawei/bean/Road.java
@@ -8,6 +8,8 @@ public class Road {
     private Cross from;
     private Cross to;
     private boolean isDuplex;
+    private int[] carsOnRoad;
+    private Road roadReverse = null;
 
     public Road(int id, int length, int speed, int channel, Cross from, Cross to, boolean isDuplex) {
         this.id = id;
@@ -17,6 +19,30 @@ public class Road {
         this.from = from;
         this.to = to;
         this.isDuplex = isDuplex;
+        this.carsOnRoad = new int[length];
+        if(isDuplex){
+            this.roadReverse = new Road(id,length,speed,channel,to,from);
+            this.roadReverse.setDuplex(isDuplex);
+            this.roadReverse.roadReverse = this;
+        }
+    }
+    public Road(int id, int length, int speed, int channel, Cross from, Cross to) {
+        this.id = id;
+        this.length = length;
+        this.speed = speed;
+        this.channel = channel;
+        this.from = from;
+        this.to = to;
+        this.carsOnRoad = new int[length];
+    }
+
+
+    public int[] getCarsOnRoad() {
+        return carsOnRoad;
+    }
+
+    public void setCarsOnRoad(int[] carsOnRoad) {
+        this.carsOnRoad = carsOnRoad;
     }
 
     public int getId() {
